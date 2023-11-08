d1aea03bb70e327dff24c86e29852b79adce36d5
mexed <thomas-loewen@hotmail.de>
2023-11-08 12:49:07 +0100

    commit to branch

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                     <div class="card-header sticky">
 						<nav class="navbar navbar-expand-lg bg-body-tertiary">
 							<div class="container-fluid">
-								<a class="navbar-brand" href="#">Thomas Loewen</a>
+								<a class="navbar-brand" href="#">Thomas L</a>
 								<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 								<span class="navbar-toggler-icon"></span>
 								</button>
